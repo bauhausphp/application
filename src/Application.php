9c91649453027dbaf5d5d433bd7ebc2aafc2275e
Felipe Martins <me@fefas.net>
2017-05-22 01:35:51 -0300

    Stack up PSR15 Middleware using string

--- src/Application.php
+++ src/Application.php
@@ -16,12 +16,31 @@ class Application
 
     public function stackUp($middleware): void
     {
-        if (!$middleware instanceof Psr15Middleware) {
+        if (false === $this->implementsPsr15Middleware($middleware)) {
             throw new InvalidArgumentException(
                 'Only PSR-15 Middlewares can be stacked up'
             );
         }
 
         $this->stack[] = $middleware;
+    }
+
+    private function implementsPsr15Middleware($middleware): bool
+    {
+        if (is_object($middleware)) {
+            return $middleware instanceof Psr15Middleware;
+        }
+
+        if (false === is_string($middleware)) {
+            return false;
+        }
+
+        if (false === class_exists($middleware)) {
+            return false;
+        }
+
+        $implementedInterfaces = class_implements($middleware);
+
+        return in_array(Psr15Middleware::class, $implementedInterfaces);
     }
 }
--- tests/unit/ApplicationTest.php
+++ tests/unit/ApplicationTest.php
@@ -6,37 +6,54 @@ use PHPUnit\Framework\TestCase;
 
 class ApplicationTest extends TestCase
 {
+    private $application;
+
+    protected function setUp()
+    {
+        $this->application = new Application();
+    }
+
     /**
      * @test
      */
     public function stackUpPsr15Middleware()
     {
-        $application = new Application();
         $middleware = new PassMiddleware();
 
-        $application->stackUp($middleware);
+        $this->application->stackUp($middleware);
 
-        $this->assertEquals([$middleware], $application->stack());
+        $this->assertEquals([$middleware], $this->application->stack());
     }
 
     /**
      * @test
+     */
+    public function stackUpPsr15MiddlewareFromString()
+    {
+        $this->application->stackUp(PassMiddleware::class);
+
+        $this->assertEquals([PassMiddleware::class], $this->application->stack());
+    }
+
+    /**
+     * @test
+     * @dataProvider notPsr15Middlewares
      * @expectedException \InvalidArgumentException
      * @expectedExceptionMessage Only PSR-15 Middlewares can be stacked up
      */
-    public function exceptionOccursWhenTryingToStackUpANotPsr15Middleware()
-    {
-        $application = new Application();
-        $notPsr15Middleware = new NotPsr15Middleware();
-
-        $application->stackUp($notPsr15Middleware);
+    public function exceptionOccursWhenTryingToStackUpANotPsr15Middleware(
+        $notPsr15Middleware
+    ) {
+        $this->application->stackUp($notPsr15Middleware);
     }
 
-    //public function notPsr15Middlewares()
-    //{
-    //    return [
-    //        [NotPsr15Middleware::class],
-    //        [new NotPsr15Middleware()],
-    //    ];
-    //}
+    public function notPsr15Middlewares()
+    {
+        return [
+            [NotPsr15Middleware::class],
+            [new NotPsr15Middleware()],
+            ['some string'],
+            [123],
+        ];
+    }
 }
